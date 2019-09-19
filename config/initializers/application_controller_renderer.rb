869aef9d5caa4f9f531e68454a89e8eb3ded00aa
ritohirade <riitonboy89love@gmail.com>
2019-09-19 20:30:11 +0900

    commitmessage

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '37bfeef9ce285eae1856b561062ecc90b8b2517c6949e696ce0f49f8e8c40e67a03577b4162e256669ef5d10da2599374941ab86a3158d17e44b99385a41c6a2'
